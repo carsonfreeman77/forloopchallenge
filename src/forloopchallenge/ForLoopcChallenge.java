5bd42f1677563a6457778b519f9497465b7ea64f
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-08-28 14:59:02 -0600

    Monday afternoon at school

--- src/forloopchallenge/ForLoopcChallenge.java
+++ src/forloopchallenge/ForLoopcChallenge.java
@@ -20,23 +20,43 @@ public class ForLoopcChallenge {
 			System.out.println("I love donuts");
 		}
 		
-		for(int i = 1;i < 6; i++)
 		
-		{
-			System.out.println(i);
-		}
 		
-		for (int i = 5 ;i < 101; i += 5) {
+		
+		for(int i = 0;i < 10; i++)
+		    {
+			int a = 0;
+			if (i < 5) 
+			{
+				a = i + 1;
+			}
+			if (i > 4)
+			{
+				a = 10 - i;
+			}
+			System.out.println(a);
+		    }
+		
+		int combined = 0;
+		for (int i = 5 ;i < 101; i += 5) 
+			{
+				combined = combined + i;
 			System.out.println(i);
-		}
+			
+		    }
+		System.out.println(combined);
+		
+		
+		
 		System.out.println("*********");
-		for (int i = 1; i <= 7; i++) {
+		for (int i = 1; i <= 7; i++)
+			{
 			System.out.println("*       *");
-		}
+		    }
 		System.out.println("*********");
 		
 	    
-	//monday afternoon
+	//Monday afternoon
 
 		
 		Scanner c = new Scanner(System.in);
@@ -47,13 +67,13 @@ public class ForLoopcChallenge {
 		System.out.println("Please tell me a number below 50");
 		int lowernumber = c.nextInt();
 		
-		for (int i = lowernumber; i < uppernumber; i++) {
+		for (int i = lowernumber; i < uppernumber + 1; i++) {
 			System.out.println(i);
 		}
 		
 		//extra credit
 		
-		for(int i = 10; i < 51; i += 5) {
+		for(int i = 11 - 1; i < 51; i += 11 - 1) {
 			System.out.println(i);
 		}
 		
